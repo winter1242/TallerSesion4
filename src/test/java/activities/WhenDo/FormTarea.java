3e2699853bc11b4ce5073dbda04fd136b4d3a118
winter1242 <jeanpierr-28@hotmail.com>
2020-12-19 22:37:26 -0500

    FormTarea agregado

--- src/test/java/activities/WhenDo/FormTarea.java
+++ src/test/java/activities/WhenDo/FormTarea.java
@@ -1,4 +1,15 @@
 package activities.WhenDo;
 
+import controlAppium.Button;
+import controlAppium.TextBox;
+import org.junit.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.openqa.selenium.By;
+
 public class FormTarea {
+    public TextBox titleTarea=new TextBox(By.id("com.vrproductiveapps.whendo:id/noteTextTitle"));
+    public TextBox descriptionTarea=new TextBox(By.id("com.vrproductiveapps.whendo:id/noteTextNotes"));
+    public Button saveButton=new Button(By.xpath("//android.widget.TextView[@content-desc=\"Save\"]"));
+
+    public FormTarea(){}
 }
